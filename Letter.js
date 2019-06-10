bf8e006fbf686c435ee6f23a79f0e77eb935c24c
Your Name <you@example.com>
2019-06-10 00:36:21 -0700

    add comments on letter.js

--- Letter.js
+++ Letter.js
@@ -1,7 +1,10 @@
 function Letter(character) {
 
+    // A string value to store the underlying character for the letter
     this.character = character;
+    // A boolean value that stores whether that letter has been guessed yet
     this.guess = false;
+    // A function that returns the underlying character if the letter has been guessed, or a placeholder (like an underscore) if the letter has not been guessed
     this.display = function () {
         if (this.guess) {
             return this.character;
@@ -10,7 +13,7 @@ function Letter(character) {
             return "_";
         }
     }
-
+    //  A function that takes a character as an argument and checks it against the underlying character, updating the stored boolean value to true if it was guessed correctly
     this.check = function (char) {
         if (char === this.character) {
             this.guess = true;
@@ -19,6 +22,7 @@ function Letter(character) {
 
 }
 
-var test = new Letter ("k");
-test.check("m");
-console.log(test.guess);
+// code test:
+// var test = new Letter ("k");
+// test.check("m");
+// console.log(test.guess);
