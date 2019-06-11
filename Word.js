65a96c4ea778b2805e47239fbcf6d9e77beea2da
Your Name <you@example.com>
2019-06-10 22:24:49 -0700

    fixed syntax bugs and tested word.js

--- Word.js
+++ Word.js
@@ -4,27 +4,27 @@ var Letter = require("./Letter");
 function Word(newWord){
     
     this.letters = [];
-    var newWordArr = newWord.split();
-    for (var i=0; i<this.newWordArr.length; i++){
-      letters.push(new Letter(newWordArr[i]));
+    var newWordArr = newWord.split("");
+    for (var i=0; i<newWordArr.length; i++){
+      this.letters.push(new Letter(newWordArr[i]));
     }
 
     this.toString = function(){
         var wordGuess = "";
         for(var i=0; i<this.letters.length; i++ ){
-            wordGuess += letters[i].display();
+            wordGuess += this.letters[i].display() + " ";
         }
         return wordGuess;
     };
 
     this.ifGuessed = function(char){
         for(var j=0; j<this.letters.length; j++){
-            letters[j].check(char);
+            this.letters[j].check(char);
         }
     }
 } 
 
-// var test = new Word("city");
-// test.ifGuessed("c");
-// console.log(test+" ");
+var test = new Word("city");
+test.ifGuessed("c");
+console.log(test.toString());
 
