f87377590e05d09a07a31ea91f3e695b4bb54e7c
Your Name <you@example.com>
2019-06-11 01:06:21 -0700

    display remaining #

--- Index.js
+++ Index.js
@@ -2,35 +2,39 @@ var Word = require("./Word");
 
 var inquirer = require('inquirer');
 inquirer
-  .prompt([
-    {
-        message: "Guess a letter!",
-        name:"letters"
-      },
-  ])
-  .then(answers => {
-    var aNewWord = "Seattle";
-    var guessACity = new Word (aNewWord);
-    guessACity.ifGuessed(answers.letters);
-    console.log(guessACity.toString());
+    .prompt([
+        {
+            message: "Guess a letter!",
+            name: "letters"
+        },
+    ])
+    .then(answers => {
+        var remaining = 5;
+        var aNewWord = "Seattle";
+        var guessACity = new Word(aNewWord);
+        guessACity.ifGuessed(answers.letters);
+        console.log(guessACity.toString());
 
-    
-    Word.prototype.checkLetter = function(char){
-        var ifLetter = false;
-        for (var j = 0; j < this.letters.length; j++) {
-            //call Letter.check function.
-           if(this.letters[j].character === char){
-               ifLetter = true;
-           }
+        // added a new Word property to check if guessed letter is in the given word, return true if the guess is correct
+        Word.prototype.checkLetter = function (char) {
+            var ifLetter = false;
+            for (var j = 0; j < this.letters.length; j++) {
+                //call Letter.check function.
+                if (this.letters[j].character === char) {
+                    ifLetter = true;
+                }
+            }
+            return ifLetter;
         }
-        return ifLetter;
-    }
 
-        if(guessACity.checkLetter(answers.letters)){
-        console.log("CORRECT!")
-    }
-    else{
-        console.log("INCORRECT!")
-    }
+       
+        if (guessACity.checkLetter(answers.letters)) {
+            console.log("CORRECT!")
+        }
+        else {
+            remaining -= 1;
+            console.log("INCORRECT!")
+            console.log(remaining + " guesses remaining!")
+        }
 
-  });
+    });
