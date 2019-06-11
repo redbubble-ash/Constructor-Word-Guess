5a4bb5d2678e8e9805778a642677bb322a07b2b6
Your Name <you@example.com>
2019-06-10 22:40:48 -0700

    added comments on word.js

--- Word.js
+++ Word.js
@@ -1,30 +1,38 @@
 var Letter = require("./Letter");
 
 
-function Word(newWord){
-    
-    this.letters = [];
-    var newWordArr = newWord.split("");
-    for (var i=0; i<newWordArr.length; i++){
-      this.letters.push(new Letter(newWordArr[i]));
-    }
+function Word(newWord) {
 
-    this.toString = function(){
+    // An array of `new` Letter objects representing the letters of the underlying word
+    this.letters = [];
+    // convert newWord from string to array
+    var newWordArr = newWord.split("");
+    for (var i = 0; i < newWordArr.length; i++) {
+        //build each Letter object based on the given newWord array and push each Letter object into the empty arrary (Word object): this.letters
+        this.letters.push(new Letter(newWordArr[i]));
+    }
+    
+    //A function that returns a string representing the word. This should call the function on each letter object (the first function defined in `Letter.js`) that displays the character or an underscore and concatenate those together.
+    this.toString = function () {
         var wordGuess = "";
-        for(var i=0; i<this.letters.length; i++ ){
+        for (var i = 0; i < this.letters.length; i++) {
+            //*this.letter[i] === Letter object, call function: Letter.display(). Concatenating returned character or "_" into string with a space " ".
             wordGuess += this.letters[i].display() + " ";
         }
         return wordGuess;
     };
-
-    this.ifGuessed = function(char){
-        for(var j=0; j<this.letters.length; j++){
+    
+    //A function that takes a character as an argument and calls the guess function on each letter object (the second function defined in `Letter.js`)
+    this.ifGuessed = function (char) {
+        for (var j = 0; j < this.letters.length; j++) {
+            //call Letter.check function.
             this.letters[j].check(char);
         }
     }
-} 
+}
 
-var test = new Word("city");
-test.ifGuessed("c");
-console.log(test.toString());
+// test:
+// var test = new Word("city");
+// test.ifGuessed("c");
+// console.log(test.toString());
 
