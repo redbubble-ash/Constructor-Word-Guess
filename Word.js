5a92eb25129eb6c70420d910d3a8353ad9a02a05
Your Name <you@example.com>
2019-06-10 22:17:18 -0700

    built word.js

--- Word.js
+++ Word.js
@@ -1,14 +1,30 @@
 var Letter = require("./Letter");
 
 
-function Word(){
+function Word(newWord){
+    
+    this.letters = [];
+    var newWordArr = newWord.split();
+    for (var i=0; i<this.newWordArr.length; i++){
+      letters.push(new Letter(newWordArr[i]));
+    }
 
-    this.letters = Letter.character;
     this.toString = function(){
-        Letter.display;
+        var wordGuess = "";
+        for(var i=0; i<this.letters.length; i++ ){
+            wordGuess += letters[i].display();
+        }
+        return wordGuess;
     };
 
-    this.guess = function(char){
-        Letter.check(char);
+    this.ifGuessed = function(char){
+        for(var j=0; j<this.letters.length; j++){
+            letters[j].check(char);
+        }
     }
 } 
+
+// var test = new Word("city");
+// test.ifGuessed("c");
+// console.log(test+" ");
+
