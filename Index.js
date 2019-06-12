e1e1b1eb8065e966335dc2ead33dad571f961d9e
Your Name <you@example.com>
2019-06-12 00:39:22 -0700

    built while loop for index.js

--- Index.js
+++ Index.js
@@ -1,63 +1,62 @@
 var Word = require("./Word");
 
 var inquirer = require('inquirer');
-inquirer
-    .prompt([
-        {
-            message: "Guess a letter!",
-            name: "letters"
-        },
-    ])
-    .then(answers => {
-        var remaining = 5;
-        var aNewWord = "Seattle";
-        var guessACity = new Word(aNewWord);
-        if (guessACity.ifGuessed(answers.letters)) {
-            console.log("CORRECT!")
+var remaining = 5;
+var aNewWord = "Seattle";
+var guessACity = new Word(aNewWord);
+
+// added a constructor Word object to check if word has been guessed
+Word.prototype.checkIfWordGuessed = function () {
+    var ifWord = true;
+    for (var j = 0; j < this.letters.length; j++) {
+        //checking if each letter has been guessed.
+        if (!this.letters[j].guess) {
+            ifWord = false;
         }
-        else {
-            remaining -= 1;
-            console.log("INCORRECT!")
-            console.log(remaining + " guesses remaining!")
-        }
-
-        console.log(guessACity.toString());
-
-
-        Word.prototype.checkIfWordGuessed = function () {
-            var ifWord = true;
-            for (var j = 0; j < this.letters.length; j++) {
-                //call Letter.check function.
-                if (!this.letters[j].guess) {
-                    ifWord = false;
-                }
-            }
-            return ifWord;
-        }
-
-       
-        if (guessACity.ifGuessed(answers.letters)) {
-            console.log("CORRECT!")
-        }
-        else {
-            remaining -= 1;
-            console.log("INCORRECT!")
-            console.log(remaining + " guesses remaining!")
-        }
-
-    });
-
-   var checkIfWordGuessed = true;
-
-    while (!checkIfWordGuessed && remaining){
-
-        for (var n=0; n<aNewWord.length; n++){
-            if(!guessACity.letters[n].guess){
-                checkIfWordGuessed = false;
-            }
-        }
-        
-                                                                                    
-
-            
     }
+    return ifWord;
+}
+
+
+while (!checkIfWordGuessed() && remaining) {
+
+    inquirer
+        .prompt([
+            {
+                message: "Guess a letter!",
+                name: "letters"
+            },
+        ])
+        .then(answers => {
+
+            if (guessACity.ifGuessed(answers.letters)) {
+                console.log("CORRECT!")
+            }
+            else {
+                remaining -= 1;
+                console.log("INCORRECT!")
+                console.log(remaining + " guesses remaining!")
+            }
+
+            console.log(guessACity.toString());
+            if (guessACity.ifGuessed(answers.letters)) {
+                console.log("CORRECT!")
+            }
+            else {
+                remaining -= 1;
+                console.log("INCORRECT!")
+                console.log(remaining + " guesses remaining!")
+            }
+
+
+
+        }
+
+        )
+}
+
+
+
+
+
+
