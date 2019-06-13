e4af91cb8721304f8530ed16ce0c14560d301679
Your Name <you@example.com>
2019-06-13 00:31:52 -0700

    added comment line

--- Index.js
+++ Index.js
@@ -7,9 +7,10 @@ var aNewWord = wordLibrary[Math.floor(Math.random() * wordLibrary.length)];
 var guessACity = new Word(aNewWord);
 
 
-
+//*build a async function, so "await" can be added for inquirer.prompt, which is asynchronous
 async function main() {
     while ((!guessACity.checkIfWordGuessed()) && (remaining > 0)) {
+        //*add "await" to wait until inquirer.promt() is complete with the function call before start another userInput in the whileloop (start a new thread)
         const userInput = await inquirer.prompt({
             message: "Guess a letter for an American city!",
             name: "letters"
